6fc226da1397e25835d4292a3d78de15d1771b4e
胡德志 <hudezhi@solo.local>
2016-08-08 10:06:04 +0800

    新建两个ViewController

---
+++ GitDemo/BViewController.swift
@@ -0,0 +1,36 @@
+//
+//  BViewController.swift
+//  GitDemo
+//
+//  Created by 胡德志 on 16/8/8.
+//  Copyright © 2016年 hu. All rights reserved.
+//
+
+import UIKit
+
+class BViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("新建的第二个文件")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
